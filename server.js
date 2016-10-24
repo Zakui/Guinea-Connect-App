0012be25682228dc1519612772b5470093e279a0
zakui <zakuijules@live.fr>
2016-10-24 09:45:24 +0000

    created a replicate design of guinea connect app

---
+++ server.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var COMMENTS_FILE = path.join(__dirname,'hospital_form.json');
+
+app.set('port',(process.env.PORT || 4000));
+
+app.use('/',express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Cache-Control','no-cache');
+    next();
+});
+
+
+app.get('/api/submitFiles',function(req,res){
+    fs.readFile(COMMENTS_FILE,function(err,data){
+        if(err){
+            console.log(err);
+            process.exit(1);
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(app.get('port'),function(){
+    console.log('server started : http://localhost:'+app.get('port')+'/');
+});
